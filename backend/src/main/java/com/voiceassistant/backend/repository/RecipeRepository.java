9b01f3e51f314b7526996c69dc258bfba03f71e0
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2025-05-16 16:32:06 +0530

    folder for fronmtwnd

--- backend/src/main/java/com/voiceassistant/backend/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.voiceassistant.backend.repository;
-
-import com.voiceassistant.backend.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
